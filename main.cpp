84d529b0e7c93effd173325678ef921c8d3a364d
MaldBaller <mald.baller@gmail.com>
2025-08-06 17:51:41 -0700

    Added character class with constructor

--- main.cpp
+++ main.cpp
@@ -9,22 +9,30 @@ int main()
     SetConfigFlags(FLAG_FULLSCREEN_MODE);  // Set fullscreen flag
     InitWindow(0, 0, "DungeonCrawler");
 
-    //int screenWidth = getWindowWidth().x;
-   // int screenHeight = getWindowWidth().y;
+
+    int screenX = GetScreenWidth();
+    int screenY = GetScreenHeight();
 
     SetTargetFPS(120);
     while (!WindowShouldClose())
     {
-
+        
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            
+        }
 
 
         // Draw calls
         BeginDrawing();
+        ClearBackground(BLACK);
 
-        ClearBackground(DARKGREEN);
 
+        DrawRectangle(screenX / 2.f - 50, screenY / 2.f - 50, 100, 100, RED);
         
         EndDrawing();
     }
+
     CloseWindow();
+    return 0;
 }
