99638afa3386c5096aa765cc0f0bfac9c9a32ee2
Bit125 <ruleisaac44@gmail.com>
2025-08-07 17:02:53 -0700

    updated class to match new character.h

--- enemy.cpp
+++ enemy.cpp
@@ -1,4 +1,4 @@
-#include "character.cpp"
+#include "character.h"
 #include <cmath>
 #include "raymath.h"
 
@@ -7,34 +7,20 @@ float FindDistance(Vector2 position1, Vector2 position2) {
     return sqrt(pow(position1.x - position2.x, 2) + pow(position1.y - position2.y, 2));
 }
 
-class enemy : public character {
+class enemy : public Character {
 public:
-    enemy(          // Constructor
-        int str,
-        int sta,
-        int itl,
-        int dex,
-        int team,
-        int lvl,
-        int weg,
-        std::string nam,
-        Vector2 pos = {0, 0},
-        Rectangle hit = {0, 0, 0, 0},
-        Texture2D tex = {},
-        float rot = 0.0f,
-        float scl = 1.0f
-    )
-    : character(str, sta, itl, dex, team, lvl, weg, nam, pos, hit, tex, rot, scl)
+    enemy(int team, int lvl, std::string nam , Vector2 pos = {0, 0}, Rectangle hit = {0,0,0,0})
+    : Character(team, lvl, nam, pos, hit)
     {
 
     }
     
-    void Draw() {
-        DrawTexture(texture, position.x, position.y, WHITE);
-    }
+    // void Draw() {
+    //     DrawTexture(texture, position.x, position.y, WHITE);
+    // }
 
     void Move(int deltaX, int deltaY) {
-        setPosition({position.x + deltaX, position.y + deltaY});
+        SetPosition({position.x + deltaX, position.y + deltaY});
     }
 
     void FollowPlayer(Vector2 playerPosition) {
@@ -44,7 +30,7 @@ public:
         Vector2 moveToMake = {0, 0};
         for(int i; i < 4; i++) {
             // iterates through possible moves to find the best one
-            if(FindDistance( Vector2Add(position, positionChanges[i]) , playerPosition) < FindDistance(position, playerPosition)){
+            if(FindDistance( Vector2Add(position, positionChanges[i]) , playerPosition) < FindDistance( Vector2Add(position, moveToMake) , playerPosition)){
                 moveToMake = positionChanges[i];
             }
         }
