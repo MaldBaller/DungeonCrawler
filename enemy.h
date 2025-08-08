c01e4f5d10ed1f1df6ca8e3802a567f782dc5b52
Bit125 <ruleisaac44@gmail.com>
2025-08-08 11:28:23 -0700

    reverted back to old pathfinding, commented out new one

--- enemy.h
+++ enemy.h
@@ -33,10 +33,23 @@ public:
     }
 
     void FollowPlayer(Vector2 playerPosition) {
-        const float speed = 1.0;
-        float angleToPlayer = GetAngleBetweenPoints(position, playerPosition);
-        Move(cos(angleToPlayer) * speed, sin(angleToPlayer) * speed);
+        const int stepDistance = 1;
+        const Vector2 positionChanges[4] = {{0, stepDistance}, {0, -stepDistance}, {stepDistance, 0}, {-stepDistance, 0}};
+
+        Vector2 moveToMake = {0, 0};
+        for(int i; i < 4; i++) {
+            // iterates through possible moves to find the best one
+            if(FindDistance( Vector2Add(position, positionChanges[i]) , playerPosition) < FindDistance( Vector2Add(position, moveToMake) , playerPosition)){
+                moveToMake = positionChanges[i];
+            }
+        }
+        Move(moveToMake.x, moveToMake.y);
     }
+    // void FollowPlayer(Vector2 playerPosition) {
+    //     const float speed = 1.0;
+    //     float angleToPlayer = GetAngleBetweenPoints(position, playerPosition);
+    //     Move(cos(angleToPlayer) * speed, sin(angleToPlayer) * speed);
+    // }
 
     void Draw(Vector2 playerPos) {
         DrawTextureEx(playerImage, {float(GetScreenWidth() / 2.f - 16 * 4) + (position.x - playerPos.x), float(GetScreenHeight() / 2.f - 16 * 4) + (position.y - playerPos.y)}, 0.f, 4.f, WHITE);
