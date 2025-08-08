0e49787be5d5153b08b5ebe5951bba1740dd67d7
Bit125 <ruleisaac44@gmail.com>
2025-08-08 14:09:07 -0700

    finished slash function

--- sword.h
+++ sword.h
@@ -26,7 +26,7 @@ public:
     void Slash(std::vector <Enemy> enemy) { // The list of enemies is in main, so the function needs to take it in
         for(int i; i < enemy.size(); i++) {
             if(enemy[i].position.x - position.x < 60 && enemy[i].position.y - position.y < 60) {
-                // TODO: damage enemy when health is implemented
+                enemy[i].health -= damage;
             }
         }
     }
