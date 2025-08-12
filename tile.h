6557471ca4e685b77edfaf691b4e729526e9d768
Bit125 <ruleisaac44@gmail.com>
2025-08-11 17:14:46 -0700

    added draw function for an individual tile, for testing purposes

--- tile.h
+++ tile.h
@@ -12,4 +12,8 @@ public:
         tileNumber = num;
         position = pos;
     }
+
+    void Draw(Vector2 playerPos) {
+        DrawTextureEx(tileImage, {float(GetScreenWidth() / 2.f - 16 * 4) + (position.x * (32*4) - playerPos.x), float(GetScreenHeight() / 2.f - 16 * 4) + (position.y * (32*4) - playerPos.y)}, 0.f, 4.f, WHITE);
+    }
 };
