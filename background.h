5491c0a13843d0eef1877d9334426880a00d9d2d
Bit125 <ruleisaac44@gmail.com>
2025-08-13 11:44:14 -0700

    i got them all

--- background.h
+++ background.h
@@ -10,14 +10,14 @@
 // see "tile reference.png" to see what the numbers mean
 
 // left first, right second
-const std::vector<std::vector<int>[2]> lrConnections = {
+const std::vector<std::vector<std::vector<int>>> lrConnections = {
     {{0, 1, 4, 12, 13}, {0, 1, 2, 10, 11}},
     {{0, 2, 6, 7, 14},  {0, 4, 8, 9, 14}},
     {{0, 3, 8, 11},     {0, 3, 7, 12}},
     {{0, 5, 9, 10},     {0, 5, 6, 13}}
 };
 // top first, bottom second
-const std::vector<std::vector<int>[2]> udConnections = {
+const std::vector<std::vector<std::vector<int>>> udConnections = {
     {{0, 1, 5, 10, 13}, {0, 1, 3, 11, 12}},
     {{0, 3, 7, 8, 14},  {0, 5, 6, 9, 14}},
     {{0, 4, 9, 12},     {0, 4, 8, 13}},
@@ -60,11 +60,11 @@ public:
 
         //find which tiles are valid in that position
         for(int i = 1; i <= 14; i++) {
-            for(const std::vector<int> j[2] : lrConnections) {
+            for(const std::vector<std::vector<int>> &j : lrConnections) {
                 if(std::find(j[0].begin(), j[0].end(), left) != j[0].end() && std::find(j[1].begin(), j[1].end(), i) != j[1].end()) {valid += 1;}
                 if(std::find(j[0].begin(), j[0].end(), i) != j[0].end() && std::find(j[1].begin(), j[1].end(), right) != j[1].end()) {valid += 1;}
             }
-            for(const std::vector<int> j[2] : udConnections) {
+            for(const std::vector<std::vector<int>> &j : udConnections) {
                 if(std::find(j[0].begin(), j[0].end(), above) != j[0].end() && std::find(j[1].begin(), j[1].end(), i) != j[1].end()) {valid += 1;}
                 if(std::find(j[0].begin(), j[0].end(), i) != j[0].end() && std::find(j[1].begin(), j[1].end(), below) != j[1].end()) {valid += 1;}
             }
