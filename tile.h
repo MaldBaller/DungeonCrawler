33feff15b0400bc3676d386e2e8599c134acee6d
Bit125 <ruleisaac44@gmail.com>
2025-08-15 10:18:01 -0700

    colliders almost work with player now

--- tile.h
+++ tile.h
@@ -31,56 +31,56 @@ public:
         tileNumber = num;
         position = pos;
         if(tileNumber == 1 || tileNumber == 14) {
-            collider1 = {0,0,0,0};
-            collider2 = {0,0,0,0};
+            collider1 = {100000,0,0,0};
+            collider2 = {100000,0,0,0};
         }
         if(tileNumber == 2) {
-            collider1 = {position.x*32 + 24, position.y*32, 8, 32};
-            collider2 = {0,0,0,0};
+            collider1 = {position.x*32*4 + 24*4, position.y*32*4, 8*4, 32*4};
+            collider2 = {100000,0,0,0};
         }
         if(tileNumber == 3) {
-            collider1 = {position.x*32, position.y*32 + 24, 32, 8};
-            collider2 = {0,0,0,0};
+            collider1 = {position.x*32*4, position.y*32*4 + 24*4, 32*4, 8*4};
+            collider2 = {100000,0,0,0};
         }
         if(tileNumber == 4) {
-            collider1 = {position.x*32, position.y*32, 8, 32};
-            collider2 = {0,0,0,0};
+            collider1 = {position.x*32*4, position.y*32*4, 8*4, 32*4};
+            collider2 = {100000,0,0,0};
         }
         if(tileNumber == 5) {
-            collider1 = {position.x*32, position.y*32, 32, 8};
-            collider2 = {0,0,0,0};
+            collider1 = {position.x*32*4, position.y*32*4, 32*4, 8*4};
+            collider2 = {100000,0,0,0};
         }
         if(tileNumber == 6) {
-            collider1 = {position.x*32, position.y*32, 32, 8};
-            collider2 = {position.x*32 + 24, position.y*32, 8, 32};
+            collider1 = {position.x*32*4, position.y*32*4, 32*4, 8*4};
+            collider2 = {position.x*32*4 + 24*4, position.y*32*4, 8*4, 32*4};
         }
         if(tileNumber == 7) {
-            collider1 = {position.x*32 + 24, position.y*32, 32, 8};
-            collider2 = {position.x*32 + 24, position.y*32, 8, 32};
+            collider1 = {position.x*32*4, position.y*32*4 + 24*4, 32*4, 8*4};
+            collider2 = {position.x*32*4 + 24*4, position.y*32*4, 8*4, 32*4};
         }
         if(tileNumber == 8) {
-            collider1 = {position.x*32 + 24, position.y*32, 32, 8};
-            collider2 = {position.x*32, position.y*32, 8, 32};
+            collider1 = {position.x*32*4, position.y*32*4 + 24*4, 32*4, 8*4};
+            collider2 = {position.x*32*4, position.y*32*4, 8*4, 32*4};
         }
         if(tileNumber == 9) {
-            collider1 = {position.x*32, position.y*32, 32, 8};
-            collider2 = {position.x*32, position.y*32, 8, 32};
+            collider1 = {position.x*32*4, position.y*32*4, 32*4, 8*4};
+            collider2 = {position.x*32*4, position.y*32*4, 8*4, 32*4};
         }
         if(tileNumber == 10) {
-            collider1 = {position.x*32 + 24, position.y*32, 8, 8};
-            collider2 = {0,0,0,0};
+            collider1 = {position.x*32*4 + 24*4, position.y*32*4, 8*4, 8*4};
+            collider2 = {100000,0,0,0};
         }
         if(tileNumber == 11) {
-            collider1 = {position.x*32 + 24, position.y*32 + 24, 8, 8};
-            collider2 = {0,0,0,0};
+            collider1 = {position.x*32*4 + 24*4, position.y*32*4 + 24*4, 8*4, 8*4};
+            collider2 = {100000,0,0,0};
         }
         if(tileNumber == 12) {
-            collider1 = {position.x*32, position.y*32 + 24, 8, 8};
-            collider2 = {0,0,0,0};
+            collider1 = {position.x*32*4, position.y*32*4 + 24*4, 8*4, 8*4};
+            collider2 = {100000,0,0,0};
         }
         if(tileNumber == 13) {
-            collider1 = {position.x*32, position.y*32, 8, 8};
-            collider2 = {0,0,0,0};
+            collider1 = {position.x*32*4, position.y*32*4, 8*4, 8*4};
+            collider2 = {100000,0,0,0};
         }
     }
 
